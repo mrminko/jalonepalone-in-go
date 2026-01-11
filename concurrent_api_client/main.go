abc6d9f021e5e2c66c1bc464e523c42b47a285e9
Min Ko Oo <minkooo2200@gmail.com>
2026-01-11 13:34:08 +0900

    add post method

--- concurrent_api_client/main.go
+++ concurrent_api_client/main.go
@@ -4,11 +4,12 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/goccy/go-yaml"
 	"io"
 	"net/http"
 	"os"
 	"sync"
+
+	"github.com/goccy/go-yaml"
 )
 
 type Api struct {
@@ -16,6 +17,7 @@ type Api struct {
 	Name     string
 	Endpoint string
 	Method   string
+	Payload  map[string]interface{}
 }
 
 type Config struct {
@@ -84,25 +86,57 @@ func fetchApi(api *Api, ch ApiResponseChannel, wg *sync.WaitGroup) {
 	defer wg.Done()
 	switch api.Method {
 	case "GET", "get":
-		resp, err := http.Get(api.Endpoint)
-		if err != nil {
-			ch <- &ApiResponse{
-				label: api.Label,
-				err:   err,
-			}
-			return
+		get(api, ch)
+	case "POST", "post":
+		post(api, ch)
+
+	}
+}
+
+func post(api *Api, ch ApiResponseChannel) {
+	jsonData, err := json.Marshal(api.Payload)
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		return
+	}
+
+	resp, err := http.Post(api.Endpoint, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		ch <- &ApiResponse{
+			label: api.Label,
+			err:   err,
 		}
-		defer resp.Body.Close()
-		raw, err := io.ReadAll(resp.Body)
-		if err != nil {
-			ch <- &ApiResponse{
-				label: api.Label,
-				err:   err,
-			}
-			return
+		return
+	}
+	handleRespBody(api, resp, ch)
+}
+
+func get(api *Api, ch ApiResponseChannel) {
+	resp, err := http.Get(api.Endpoint)
+	if err != nil {
+		ch <- &ApiResponse{
+			label: api.Label,
+			err:   err,
 		}
+		return
+	}
+	handleRespBody(api, resp, ch)
+}
+
+func handleRespBody(api *Api, resp *http.Response, ch ApiResponseChannel) {
+	defer resp.Body.Close()
+	var body []byte
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		ch <- &ApiResponse{
+			label: api.Label,
+			err:   err,
+		}
+		return
+	}
+	if resp.Header.Get("content-type") != "application/json" {
 		buf := &bytes.Buffer{}
-		err = json.Indent(buf, raw, "", "  ")
+		err = json.Indent(buf, body, "", "  ")
 		if err != nil {
 			ch <- &ApiResponse{
 				label: api.Label,
@@ -113,6 +147,12 @@ func fetchApi(api *Api, ch ApiResponseChannel, wg *sync.WaitGroup) {
 		ch <- &ApiResponse{
 			label:  api.Label,
 			result: buf.String(),
+			err:    nil,
+		}
+	} else {
+		ch <- &ApiResponse{
+			label:  api.Label,
+			result: string(body),
 			err:    nil,
 		}
 	}
